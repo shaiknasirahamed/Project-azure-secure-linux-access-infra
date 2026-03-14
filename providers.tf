158163677a6f5a082a37d2c4aca1fe788c0da40a
shaiknasirahamed <107348078+shaiknasirahamed@users.noreply.github.com>
2026-03-14 16:36:07 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.113.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
