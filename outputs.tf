b138cb2306be83b1a720d5e5c08a7fe589123b74
shaiknasirahamed <107348078+shaiknasirahamed@users.noreply.github.com>
2026-03-14 16:40:56 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
 
+output "vnet_name" {
+  value = module.network.vnet_name
+}
+
+output "subnet_app_id" {
+  value = module.network.subnet_app_id
+}
+
+output "subnet_bastion_id" {
+  value = module.network.subnet_bastion_id
+}
